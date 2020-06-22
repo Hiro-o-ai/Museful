52065b5c3c68bf5f4144e5aa80394475097d6405
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-06-22 02:22:40 +0000

    adjust_code

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,10 @@
 class TopsController < ApplicationController
   def top
+    @favorite_articles
+    @bookmark_articles
+    @avarage_rate_articles
+
+    @new_questions
+    @respond_questions
   end
 end
