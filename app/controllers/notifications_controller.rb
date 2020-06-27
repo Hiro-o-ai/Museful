12ffbea1e538156a1952a93c5db437cfc934e352
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-06-27 13:54:31 +0000

    fix_notification_controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,4 +1,8 @@
 class NotificationsController < ApplicationController
+  def index
+    @notificatons = current_user.notifications.order(created_at: :desc)
+  end
+
   def update
     notification = current_user.notifications.find(params[:id])
     # read!はupdate(read: true)と同じ処理をする
