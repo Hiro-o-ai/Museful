a57e96c486047bb38f6d6b649fada4684d2952bb
Hiro-o-ai <110434009@ccalumni.meijo-u.ac.jp>
2020-07-26 15:25:04 +0900

    fix_readme

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,9 +35,6 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   # factorybotとテスト内に入力せずに使用できるようにする
   config.include FactoryBot::Syntax::Methods
-  # deviseのテストに必要
-  config.include Devise::Test::IntegrationHelpers, type: :request
-
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
